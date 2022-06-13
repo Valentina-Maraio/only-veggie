f6ee0e12714787f8098e4a78e840829d0566b342
Valentina Maraio <valentina.maraio89@gmail.com>
2022-06-13 22:41:51 +0200

    change

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,33 @@
 import React, {useState} from 'react'
 import { InputText } from 'primereact/inputtext'
+import Data from '../data/food.json'
+import '../App.css';
 
 const Search = () => {
     const [value, setValue] = useState('')
 
   return (
     <div>
-        <InputText value={value} onChange={(e) => setValue(e.target.value)} />
+        <InputText
+        value={value}
+        />
+        {
+          Data.filter(ricetta => () => {
+            if(value === '') {
+              return ricetta;
+            } else if (ricetta.title.toLowerCase().match(value.toLocaleLowerCase())){
+              return ricetta;
+            }
+          }).map((ricetta) => (
+            <div
+            className="box"
+            key={ricetta.id}>
+              <p>
+                {ricetta.name}
+              </p>
+            </div>
+          ))
+        }
     </div>
 
   )
